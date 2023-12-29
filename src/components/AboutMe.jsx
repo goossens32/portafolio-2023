b5d64516d08633ca1c65bd566864f6c8befb81b8
goossens32 <cgp00@iesemilidarder.com>
2023-12-29 21:58:53 +0100

    Added my projects

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,17 +2,24 @@ import './AboutMe.css';
 
 const AboutMe = () => {
     return (
-        <div className="aboutme-container">
-            <div className="introduction">
-                <h2>¡Hola! Soy Carlos,</h2>
-                <p>
-                ¡Bienvenido a mi portafolio! Soy Carlos, un apasionado del desarrollo web que combina creatividad y código para dar vida a experiencias online cautivadoras. A lo largo de mi trayectoria, he convertido líneas de código en sitios web dinámicos y funcionales que no solo cumplen objetivos, sino que también cautivan y sorprenden a los usuarios.
-                </p>
+        <div>
+            <div className="aboutme-container">
+                <div className="introduction">
+                    <h2>¡Hola! Soy Carlos,</h2>
+                    <p>
+                    ¡Bienvenido a mi portafolio! Soy Carlos, un apasionado del desarrollo web que combina creatividad y código para dar vida a experiencias online cautivadoras. A lo largo de mi trayectoria, he convertido líneas de código en sitios web dinámicos y funcionales que no solo cumplen objetivos, sino que también cautivan y sorprenden a los usuarios.
+                    </p>
+                </div>
+                <div className="avatar">
+                    <img src={require(`../img/myAvatar.png`)} alt='' />
+                </div>
             </div>
-            <div className="avatar">
-                <img src={require(`../img/myAvatar.png`)} alt='' />
+            <div className='btn-see-projects'>
+                <a href="#projects"><button>VER PROYECTOS</button></a>
+                
             </div>
         </div>
+        
     )
 }
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,8 +2,95 @@ import './Projects.css';
 
 const Projects = () => {
     return (
-        <div>
-            PROYECO
+        <div className='projects-container' id='projects'>
+            <h2>Proyectos realizados</h2>
+            <div className='projects-list'>
+                <div className='project-item'>
+                    <div className='project-logo'>
+                        <img src={require(`../img/logo-botchat.png`)} alt='' />
+                    </div>
+                    <div className='project-info'>
+                        <p>Plataforma para contratar un Botchat para pequeña o medianas empresas en la que, permite automatizar mensajes de soporte. Soporte Instantáneo: Proporciona asistencia a tus clientes las 24 horas del día, los 7 días de la semana.</p>
+                        <h4>Tecnologías utilizadas</h4>
+                        <div className='see-more-container'>
+                            <div className='technology'>
+                                <i class="fa-brands fa-html5"></i>
+                                <i class="fa-brands fa-css3-alt"></i>
+                                <i class="fa-brands fa-js"></i>
+                                <i class="fa-brands fa-react"></i>
+                            </div>
+                            <div className='btn-see-more'>
+                                 <a href="https://cgp00-botchat.netlify.app/" target="_blank" rel='noreferrer'><button>VER MÁS</button></a>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div className='project-item'>
+                    <div className='project-logo' id='mallorcar-logo'>
+                        <img src={require(`../img/logo-mallorcar.png`)} alt='' />
+                    </div>
+                    <div className='project-info'>
+                        <p>Página web destinada a la compraventa de vehículos de ocasión y segunda mano. Permite ver el listado de vehículos a la venta y contactar con el vendedor para su posterior compra. Esta página web ya és dinámica utilizando PHP.</p>
+                        <h4>Tecnologías utilizadas</h4>
+                        <div className='see-more-container'>
+                            <div className='technology'>
+                                <i class="fa-brands fa-html5"></i>
+                                <i class="fa-brands fa-css3-alt"></i>
+                                <i class="fa-brands fa-js"></i>
+                                <i class="fa-brands fa-react"></i>
+                            </div>
+                            <div className='btn-see-more'>
+                                 <a href="https://mallorcar-cgp00.netlify.app/" target="_blank" rel='noreferrer'><button>VER MÁS</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='project-item'>
+                    <div className='project-logo' id='mallorcar-logo'>
+                        <img src={require(`../img/logo-mediamarkt.png`)} alt='' />
+                    </div>
+                    <div className='project-info'>
+                        <p>Proyecto en conjunto con la tienda online de Media-Markt, mi parte era desarrollar el diseño de las cards para cada item en venta junto con la funcionalidad para añadir al carrito. Un trabajo laborioso y uno de los primeros en mi carrera profesional</p>
+                        <h4>Tecnologías utilizadas</h4>
+                        <div className='see-more-container'>
+                            <div className='technology'>
+                                <i class="fa-brands fa-html5"></i>
+                                <i class="fa-brands fa-css3-alt"></i>
+                                <i class="fa-brands fa-js"></i>
+                                <i class="fa-brands fa-react"></i>
+                            </div>
+                            <div className='btn-see-more'>
+                                 <a href="https://mediamarkt-cgp00.netlify.app/" target="_blank" rel='noreferrer'><button>VER MÁS</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='project-item'>
+                    <div className='project-logo' id='amazon-logo'>
+                        <img src={require(`../img/logo-amazonprime.png`)} alt='' />
+                    </div>
+                    <div className='project-info'>
+                        <p>Parte de mi actual trabajo en Amazon. Junto con mis compañeros mi trabajo consiste mantener la página web de Amazon Prime Video junto con añadir las nuevas películas/series y sus </p>
+                        <h4>Tecnologías utilizadas</h4>
+                        <div className='see-more-container'>
+                            <div className='technology'>
+                                <i class="fa-brands fa-html5"></i>
+                                <i class="fa-brands fa-css3-alt"></i>
+                                <i class="fa-brands fa-js"></i>
+                                <i class="fa-brands fa-react"></i>
+                            </div>
+                            <div className='btn-see-more'>
+                                 <a href="https://cgp00-primevideo.netlify.app/" target="_blank" rel='noreferrer'><button>VER MÁS</button></a>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                
+
+            </div>
         </div>
     )
 }
